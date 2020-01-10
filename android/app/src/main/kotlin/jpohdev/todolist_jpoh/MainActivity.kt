c6d80ee062bf135ff2705ad609829dcb3f345ef9
JongPyo <hjp4869@naver.com>
2020-01-10 16:34:05 +0900

    2020-01-10 We'll do Project 1일차 Main App bar

---
+++ android/app/src/main/kotlin/jpohdev/todolist_jpoh/MainActivity.kt
@@ -0,0 +1,12 @@
+package jpohdev.todolist_jpoh
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
